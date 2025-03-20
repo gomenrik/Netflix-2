d9e35d3118501e3b7209424282ace9b41418f195
gomenrik <campanhahenrique13@gmail.com>
2025-03-20 20:09:29 -0300

    index.html

---
+++ js/login.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", asyn () => {
+    setTimeout(() => {
+      toggleLoading();
+    }, 2000);
+});
--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
+// Show Loading
 function toggleLoading() {
     let loader = document.querySelector(".loader");
     loader.style.display = loader.style.display == "none" ? "block" : "none";
 }
+
+// Page Scroll
+window.addEventListener("scroll", function () {
+    const navbar = this.document.querySelector(".navbar");
+
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+});
